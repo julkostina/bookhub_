710bdab04dde684e7ec047d478a7abac659cd105
julkostina <julikostina1504@gmail.com>
2026-07-05 20:16:46 +0300

    Add basename to BrowserRouter for GitHub Pages subpath routing

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/bookhub_">
     <App />
     </BrowserRouter>
   </React.StrictMode>
